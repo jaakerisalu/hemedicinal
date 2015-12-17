fd6551ff5edb80fbfbe06f7d377d8f9a01a04cf2
Jaak Erisalu <jaak.erisalu@gmail.com>
2015-12-17 19:57:12 +0200

    Remove deprecated stuffs from Admin

--- hemedicinal/hemedicinal/admin.py
+++ hemedicinal/hemedicinal/admin.py
@@ -1,7 +1,7 @@
 from hemedicinal.models import Drug, Supplier, Manufacturer, Substance
 from django.contrib import admin
 
-admin.site.register(Drug)
-admin.site.register(Supplier)
-admin.site.register(Manufacturer)
-admin.site.register(Substance)
+# admin.site.register(Drug)
+# admin.site.register(Supplier)
+# admin.site.register(Manufacturer)
+# admin.site.register(Substance)
